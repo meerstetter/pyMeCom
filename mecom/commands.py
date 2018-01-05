a94eb3a8caa9b20563363df51db65ea7dcce41f5
Suthep Pomjaksilp <git@laz0r.de>
2018-01-05 09:50:08 +0100

    ported updated commands from internal repository.

--- mecom/commands.py
+++ mecom/commands.py
@@ -6,15 +6,28 @@ https://www.meerstetter.ch/category/35-latest-communication-protocols
 
 PARAMETERS = [
     {"id": 104, "name": "Device Status", "format": "INT32"},
-    {"id": 2010, "name": "Status", "format": "INT32"},
-    {"id": 1010, "name": "Target Object Temperature", "format": "FLOAT32"},
+    {"id": 108, "name": "Save Data to Flash", "format": "INT32"},
+    {"id": 109, "name": "Flash Status", "format": "INT32"},
     {"id": 1000, "name": "Object Temperature", "format": "FLOAT32"},
+    {"id": 1001, "name": "Sink Temperature", "format": "FLOAT32"},
+    {"id": 1010, "name": "Target Object Temperature", "format": "FLOAT32"},
+    {"id": 1011, "name": "Ramp Object Temperature", "format": "FLOAT32"},
+    {"id": 1020, "name": "Actual Output Current", "format": "FLOAT32"},
+    {"id": 1021, "name": "Actual Output Voltage", "format": "FLOAT32"},
     {"id": 1200, "name": "Temperature is Stable", "format": "INT32"},
-    {"id": 2051, "name": "Device Address", "format": "INT32"}
+    {"id": 2010, "name": "Status", "format": "INT32"},
+    {"id": 2051, "name": "Device Address", "format": "INT32"},
+    {"id": 3000, "name": "Target Object Temp (Set)", "format": "FLOAT32"},
+    {"id": 6310, "name": "Delay till Restart", "format": "FLOAT32"},
 ]
-
 
 ERRORS = [
     {"code": 1, "symbol": "EER_CMD_NOT_AVAILABLE", "description": "Command not available"},
-    {"code": 2, "symbol": "EER_DEVICE_BUSY", "description": "Device is busy"}
+    {"code": 2, "symbol": "EER_DEVICE_BUSY", "description": "Device is busy"},
+    {"code": 3, "symbol": "ERR_GENERAL_COM", "description": "General communication error"},
+    {"code": 4, "symbol": "EER_FORMAT", "description": "Format error"},
+    {"code": 5, "symbol": "EER_PAR_NOT_AVAILABLE", "description": "Parameter is not available"},
+    {"code": 6, "symbol": "EER_PAR_NOT_WRITABLE", "description": "Parameter is read only"},
+    {"code": 7, "symbol": "EER_PAR_OUT_OF_RANGE", "description": "Value is out of range"},
+    {"code": 8, "symbol": "EER_PAR_INST_NOT_AVAILABLE", "description": "Parameter is read only"},
 ]
