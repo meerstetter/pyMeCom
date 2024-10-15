3ea6d9b81f51599754e6c468cf3207f56f8443bc
Samir Ceka <samir.ceka@meerstetter.ch>
2024-10-15 17:11:55 +0200

    Add an option to discard initial data when establishing a TCP connection
    this is currently only needed for the LTR-1200 devices
    since they send a welcome message on connect

--- mecom/mecom.py
+++ mecom/mecom.py
@@ -7,6 +7,7 @@ from functools import partialmethod
 import time
 from threading import Lock
 import socket
+import select
 
 # more special pip packages
 from serial import Serial
@@ -831,17 +832,28 @@ class MeComTcp(MeComCommon):
     """
     SEQUENCE_COUNTER = 1
 
-    def __init__(self, ipaddress, ipport=50000, metype='TEC'):
+    def __init__(self, ipaddress, ipport=50000, discardwait=None, metype='TEC'):
         """
-        Initialize communication with TCP connection.
+        Initialize a TCP connection. Use the discardwait parameter for devices which send a message on connect, like the LTR-1200.
         :param ipaddress: str
         :param ipport: int
         :param timeout: int
+        :param discardwait: int: waits at most for the specified amount of seconds for initial data to arrive, then discards it
         :param metype: str: either 'TEC', 'LDD-112x', 'LDD-130x' or 'LDD-1321'
         """
         # initialize network connection
         self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp.connect((ipaddress, ipport))
+
+        # if configured, discard any data received right after connecting
+        if discardwait is not None:
+            # wait for data to arrive
+            readable, _, _ = select.select([self.tcp], [], [], discardwait)
+
+            # read from the socket until the buffer is empty
+            while self.tcp in readable:
+                self.tcp.recv(1024)
+                readable, _, _ = select.select([self.tcp], [], [], 0)
 
         # initialize parameters
         self.PARAMETERS = ParameterList(metype)
